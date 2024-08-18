71a278db08d6a202ba105fd3bd2e32fb9dcd3435
diazemilianoariel <emiliano.diaz@alumnos.frgp.utn.edu.ar>
2024-08-18 13:11:02 -0300

    se agregaron clases y se agrego la base de datos

---
+++ dominio/Factura.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace dominio
+{
+    public class Factura
+    {
+        // esta clase registrará las facturas de las ventas:
+        public int IdFactura { get; set; }
+        public int IdVenta { get; set; }
+        public int idCliente { get; set; }
+        public DateTime Fecha { get; set; }
+        public decimal TotalFacutra{ get; set; }
+        public decimal subTotalFactura { get; set; }
+        public decimal Iva { get; set; }
+        public decimal Descuento { get; set; }
+        
+
+    }
+}
---
+++ dominio/Perfil.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace dominio
+{
+    public class Perfil
+    {
+        // esta clase registrará los perfiles tanto de administradores como de vendedores, tipo de perfil Enum:
+
+        public enum TipoPerfil
+        {
+            Administrador,
+            Vendedor
+        }
+        public int IdPerfil { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string Estado { get; set; }
+    }
+
+}
